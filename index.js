81473a2c0d2d7d4cdfe8f31ed1ad5d635764a57f
Karin <122797442+kkarrinn@users.noreply.github.com>
2023-01-30 15:09:52 +0200

    add

--- index.js
+++ index.js
@@ -2,12 +2,40 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
+const nunjucks = require('nunjucks');
 
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+});
 app.get('/', (req, res) => {
-    //console.log(path.resolve(__dirname, 'index.html'));
   res.sendFile(path.resolve(__dirname,'index.html'));
+  res.render('index.njk');
 })
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.resolve(__dirname,'index.html'));
+  res.render('about.njk');
+}) 
+
+app.get('/gallery', (req, res) => {
+  res.sendFile(path.resolve(__dirname,'index.html'));
+  res.render('gallery.njk');
+})
+
+app.get('/contact', (req, res) => {
+  res.sendFile(path.resolve(__dirname,'index.html'));
+  res.render('contact.njk');
+})
+
+app.get('/values', (req, res) => {
+  res.sendFile(path.resolve(__dirname,'index.html'));
+  res.render('values.njk');
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 })
+
+
